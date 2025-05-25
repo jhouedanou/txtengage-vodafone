3ead9a8f07ca9dd388ff19a555cf4f8f00ae2fc1
Jean Luc Houedanou <jeanluc@houedanou.com>
2025-05-25 22:15:31 +0000

    Update SCSS background image paths to use absolute URLs for consistency across components. This change ensures that images are correctly referenced regardless of the current directory structure, enhancing visual integrity in the layout.

---
+++ .vercel/output/functions/__nitro.func/chunks/build/client.manifest.mjs
@@ -0,0 +1,108 @@
+const client_manifest = {
+  "_DCBd3tyy.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DCBd3tyy.js",
+    "name": "vue.8fc199ce",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Wr_55eUq.js",
+    "name": "error-404",
+    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DCBd3tyy.js"
+    ],
+    "css": [
+      "error-404.CoZKRZXM.css"
+    ]
+  },
+  "error-404.CoZKRZXM.css": {
+    "file": "error-404.CoZKRZXM.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "z7fhW8El.js",
+    "name": "error-500",
+    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DCBd3tyy.js"
+    ],
+    "css": [
+      "error-500.D6506J9O.css"
+    ]
+  },
+  "error-500.D6506J9O.css": {
+    "file": "error-500.D6506J9O.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DlgIYFO5.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.DmJZ35JK.css"
+    ]
+  },
+  "entry.DmJZ35JK.css": {
+    "file": "entry.DmJZ35JK.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DuLPdFmO.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DCBd3tyy.js"
+    ],
+    "css": [
+      "index.C1F-n3Ev.css"
+    ]
+  },
+  "index.C1F-n3Ev.css": {
+    "file": "index.C1F-n3Ev.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
